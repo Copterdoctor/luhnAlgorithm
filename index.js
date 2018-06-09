88187cd0c8e237a4c1e5fa7840d589fa6f5338a3
Copterdoctor <jordananders@me.com>
2018-06-09 17:03:53 +0000

    Tests written and algorithm completed

---
+++ index.js
@@ -0,0 +1,22 @@
+var luhnNumber = function (num) {
+  var numArray = num.toString().split("");
+  for (let i = numArray.length - 2; i > - 1; i -= 2) {
+    var doubleNum = Number(numArray[i]) * 2;
+    if (doubleNum > 9) {
+      doubleNum -= 9;
+      numArray[i] = doubleNum;
+    }
+    if (doubleNum < 10) {
+      numArray[i] = doubleNum;
+    }
+  }
+  var total = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    total += Number(numArray[i]);
+  }
+  return total % 10 === 0;
+}
+
+module.exports = luhnNumber;
+
+console.log(luhnNumber(6226995465300412));
